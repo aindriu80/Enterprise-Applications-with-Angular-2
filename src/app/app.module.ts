f210cca776690d3f89b83cf8cb501872e5890398
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-06-11 11:42:03 +0100

    Rendering list from Firebase database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule} from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAOD872mFctgQiDOVdAdZwG7eGRljhJbLM",
+    authDomain: "pepper-47154.firebaseapp.com",
+    databaseURL: "https://pepper-47154.firebaseio.com",
+    projectId: "pepper-47154",
+    storageBucket: "pepper-47154.appspot.com",
+    messagingSenderId: "1011251873128"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
