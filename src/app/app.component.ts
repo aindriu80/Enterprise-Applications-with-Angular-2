cbda67220674c6d7df0a2ca81175cbaef9456faf
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-06-12 10:07:42 +0100

    orderbyKey orderbyValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,8 +20,16 @@ export class AppComponent {
   }
 
   ngOnInit(){
-  this.cuisines = this.af.database.list('/cuisines');
-  this.restaurants = this.af.database.list('/restaurants');
+  this.cuisines = this.af.database.list('/cuisines',{
+    query: {
+      orderByValue: true
+    }
+  });
+  this.restaurants = this.af.database.list('/restaurants',{
+    query:{
+      orderByChild: 'address/city'
+    }
+  });
 
    // /restaurants/1/features/1
    this.exists = this.af.database.object('/restaurants/1/features/1');
